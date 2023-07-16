fa2f01da74afcc07142c24f0dd88151019b88597
Cody <ccoff1798@gmail.com>
2023-07-15 23:17:52 -0600

    Added intial prompt in genearte Password Function

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
-function writePassword() {
+function writePassword(criteria1) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+}
+function generatePassword(generateReturn){
+  var length = prompt("Pick password length")
+  // if(length >= 8 $$ length <=128){
+  //   console.log("debug")
+  // } else{
+  //   console.log("ElseDebug")
+  // }
+  var generateReturn = length
+  console.log(length)
 
 }
 
