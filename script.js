31beb35e8cd2857a8332882ad2a3f4f0fdb77edc
Cody <ccoff1798@gmail.com>
2023-07-16 12:05:58 -0600

    Generate Return added to with second criteria

--- script.js
+++ script.js
@@ -8,15 +8,28 @@ function writePassword(criteria1) {
 
 }
 function generatePassword(generateReturn){
-  var length = prompt("Pick password length")
-  // if(length >= 8 $$ length <=128){
-  //   console.log("debug")
+  var specialCharacters = "Override"
+  var length = prompt("Pick password length between 8 and 128")
+  if(length >= 8){
+    if(length < 128){
+      specialCharacters = prompt("Would you like to include special characters? Y/N")
+      if(specialCharacters == "Y"){
+        console.log(specialCharacters)
+      }
+      if(specialCharacters == "N")
+      {
+        console.log(specialCharacters)
+      }
+    }
+    
+  }else{
+    alert("You did Bad")
+  }
   // } else{
   //   console.log("ElseDebug")
   // }
   var generateReturn = length
   console.log(length)
-
 }
 
 // Add event listener to generate button
